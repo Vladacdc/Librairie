f9bc8b4e3fafc3be89a3daa4abfd74aecea29c6e
Vlad <38084885+Vladacdc@users.noreply.github.com>
2019-05-10 21:58:49 +0300

    Added db model Added login via google Added adding users to db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,15 +1,26 @@
 const passport = require('passport');
 const googleAuth = require('passport-google-oauth20').Strategy;
-const keys = require('./keys.js');
+const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
   new googleAuth({
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log('callback from google');
-    console.log(profile);
-  })
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({
+        googleId: profile.id
+      }).then((currentUser) => {
+        console.log('user is: ' + currentUser);
+      }).catch(() => {
+        new User({
+          username: profile.displayName,
+          googleId: profile.id
+        }).save().then((newUser) => {
+          console.log('SYSTEM: new user created' + newUser);
+        });
+      });
+    })
 );
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const  express = require('express');
-const authRoutes= require('./routes/auth-routes.js');
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 var app = express();
 
+//setup view engine
 app.set('view engine', 'ejs');
 
-app.use('/auth',authRoutes);
+//connect to db
+mongoose.connect(keys.mongodb.dbURI, {
+  useNewUrlParser: true
+}, () => {
+  console.log('Connected to db');
+});
 
+//set up routes
+app.use('/auth', authRoutes);
+
+//root route
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
