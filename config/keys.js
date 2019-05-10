62f457d18f59dfeebea6ab11951bd976f5940e57
Vlad <38084885+Vladacdc@users.noreply.github.com>
2019-05-11 00:02:39 +0300

    Minor Updates

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+        clientID: '412757050736-jnpbs4gdp1m2q9nmgtv0t4poom47ht9k.apps.googleusercontent.com',
+        clientSecret: '7SMcVlpU4VOPyBAkM5ykOt3G'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://dbAdmin:IphaiHeb5@dblibrarie-2xs8t.mongodb.net/test?retryWrites=true'
+    }
+};
