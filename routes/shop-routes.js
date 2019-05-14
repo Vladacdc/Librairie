bd7c96f58f601699f8c9f7abbeb0fc689e82784f
Vlad <38084885+Vladacdc@users.noreply.github.com>
2019-05-14 14:29:08 +0300

    Small Fix

--- routes/shop-routes.js
+++ routes/shop-routes.js
@@ -2,17 +2,12 @@ const router = require('express').Router();
 const Product = require('../models/product-model');
 
 router.get("/", (req, res) => {
-  Product.find((err, products) => {
-    var productChunks = [];
-    var chunkSize = 3;
-    for (var i = 0; i < products.length; i += chunkSize) {
-      productChunks.push(products.slice(i, i + chunkSize));
-    }
+  Product.find((err, products) => {    
     res.render("shop", {
       user: req.user,
       home: "",
       shop: "active",
-      products: productChunks
+      products: products
     });
   });
 });
